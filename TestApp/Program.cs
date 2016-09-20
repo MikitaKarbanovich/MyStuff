887f1b3f1d0ca4176b2b0c3c39825ec12cbf5409
MikitaKarbanovich <nikitakarbanovich@gmail.com>
2016-09-20 16:51:23 +0300

    point e.

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,27 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            Console.Write("123");
+            NumberAction numberAction = new NumberAction();
+            string keyboardInput;
+            int first,second;
+            Console.WriteLine("Enter first number");
+            keyboardInput=Console.ReadLine();
+            while (!Int32.TryParse(keyboardInput, out first))
+            {
+                Console.WriteLine("Please, try again. The value should be integer.");
+                keyboardInput = Console.ReadLine();
+            }
+            Console.WriteLine("Enter second number");
+            keyboardInput = Console.ReadLine();
+            while (!Int32.TryParse(keyboardInput, out second))
+            {
+                Console.WriteLine("Please, try again. The value should be integer.");
+                keyboardInput = Console.ReadLine();
+            }
+            Console.WriteLine("Result of addition:" + numberAction.Addition(first, second));
+            Console.WriteLine("Result of subtraction:" + numberAction.Subtraction(first, second));
+            Console.WriteLine("Result of multiplication:" + numberAction.Multiplication(first, second));
+            Console.WriteLine("Result of division:" + numberAction.Division(first, second));
         }
     }
 }
